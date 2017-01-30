de1ebbcd2035633f4c83d2f70d89514933847ab4
johan larsson <johan.larsson@ub.gu.se>
2017-01-30 16:21:11 +0100

    removed root controller and test

--- app/controllers/root_controller.rb
+++
@@ -1,5 +0,0 @@
-class RootController < ApplicationController
-  def index
-    redirect_to '/index'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-	root to: 'root#index'
 	mount_ember_app :frontend, to: "/"
 end
