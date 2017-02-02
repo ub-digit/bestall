8da28a3a07de254db4197f6884f997223114883f
johan larsson <johan.larsson@ub.gu.se>
2017-02-02 17:48:33 +0100

    added support for lang and logo and footer

---
+++ frontend/app/locales/en/translations.js
@@ -0,0 +1,35 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+
+	"home": {
+		"headers": {
+			"logoPrintUrl": '/gu_logo_en_high.png',
+			"level1": 'Gothenburg University Library',
+			"level2": 'Interlibrary loans',
+			"mainHeader": 'Order articles and interlibrary loans',
+			"langLink": 'På svenska',
+			"langLinkUrl": '/'
+		},
+		"footer": {
+			"content": '© <a title="Göteborgs universitet" href="http://www.gu.se/">University of Gothenburg, Sweden</a><br>Box 100, S-405 30 Gothenburg<br>Phone +46 31-786 0000, <a title="Contact" href="http://www.gu.se/omuniversitetet/kontakt/">Contact</a>'
+		},
+	},
+
+	"components": {
+		"toggle-lang": {
+			"language": {
+				"sv": "Svenska",
+				"en": "English",
+			}
+		}	
+	}
+};
+
---
+++ frontend/app/locales/sv.js
@@ -0,0 +1,16 @@
+export default {
+	home: {
+			headers: {
+				logoPrintUrl: '/gu_logo_sv_high.png',
+				level1: 'Göteborgs universitetsbibliotek',
+				level2: 'Fjärrlån',
+				mainHeader: 'Beställ artiklar och fjärrlån',
+				langLink: 'In English',
+				langLinkUrl: '/index_en.html'
+			},
+			footer: {
+				content: '© <a title="Göteborgs universitet" href="http://www.gu.se/">Göteborgs universitet</a><br>Box 100, 405 30 Göteborg<br>Tel. 031-786 0000, <a title="Kontakta oss" href="http://www.gu.se/omuniversitetet/kontakt/">Kontakta oss</a>'
+			},
+	}
+
+};
