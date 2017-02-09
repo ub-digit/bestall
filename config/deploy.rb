b6884556da8f3ef3ddf6075e5ee3d0ed920ddb5b
xrasto <Torgny.Rasmark@ub.gu.se>
2017-02-09 14:23:52 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ def deploy_config
 end
 
 # Copied into /{app}/shared/config from respective sample file
-set :linked_files, ['config/database.yml']
+set :linked_files , %w{config/database.yml config/config_secret.yml}
 
 server deploy_config['host'], user: deploy_config['user'], roles: ['app', 'db', 'web'], port: deploy_config['port']
 
