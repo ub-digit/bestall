ac253f22fc9211a4cc8e825f3371fc8ed35588c0
Magnus Kull <magnus.kull@ub.gu.se>
2017-02-17 12:41:27 +0100

    biblio controller responds with error when no items can be ordered

--- frontend/app/models/biblio.js
+++ frontend/app/models/biblio.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	title: DS.attr(),
-	canBeOrdered: DS.attr(),
+	canBeBorrowed: DS.attr(),
 	author: DS.attr(),
 	items: DS.hasMany('item')
 });
