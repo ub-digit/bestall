778b6f3401bce416c00a96fc7b11761feb28a52c
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2017-02-21 16:49:55 +0100

    Update user.js

--- frontend/app/adapters/user.js
+++ frontend/app/adapters/user.js
@@ -3,6 +3,6 @@ import ApplicationAdapter from './application';
 export default ApplicationAdapter.extend({
   urlForQueryRecord(query, modelName) {
     let baseUrl = this.buildURL(modelName);
-    return `${baseUrl}/current/`;
+    return `${baseUrl}/current`;
   }
 });
