35ee1a73f65aafce24f6f545d38e5619062522a1
Johan Andersson von Geijer <Johan.Andersson.von.geijer@ub.gu.se>
2017-02-21 15:56:35 +0100

    Added tests for missing parameters in reserves_controller

--- app/controllers/api/reserves_controller.rb
+++ app/controllers/api/reserves_controller.rb
@@ -6,21 +6,21 @@ class Api::ReservesController < ApplicationController
     itemnumber = params[:item_id]
     loantype = params[:loan_type]
 
-    # TODO use loantype, but with correct name as it appears in Koha.
-    # TODO: Error handling
-    #error_msg(ErrorCodes::VALIDATION_ERROR, "user_id is required") if borrowernumber.blank?
-    #error_msg(ErrorCodes::VALIDATION_ERROR, "location_id is required") if branchcode.blank?
-    #error_msg(ErrorCodes::VALIDATION_ERROR, "biblio_id is required") if biblionumber.blank?
+    error_msg(ErrorCodes::VALIDATION_ERROR, "user_id is required") if borrowernumber.blank?
+    error_msg(ErrorCodes::VALIDATION_ERROR, "location_id is required") if branchcode.blank?
+    error_msg(ErrorCodes::VALIDATION_ERROR, "biblio_id is required") if biblionumber.blank?
     #error_msg(ErrorCodes::VALIDATION_ERROR, "item_id is required") if itemnumber.blank?
-    #error_msg(ErrorCodes::VALIDATION_ERROR, "loan_type is required") if loantype.blank?
-    # Check the parameters and return error if params are not correct
+    error_msg(ErrorCodes::VALIDATION_ERROR, "loan_type is required") if loantype.blank?
 
-    reserve = Reserve.add(borrowernumber: borrowernumber, branchcode: branchcode, biblionumber: biblionumber, itemnumber: itemnumber)
-    if reserve
-      @response[:reserve] = reserve.as_json
-    else
-      error_msg(ErrorCodes::OBJECT_ERROR, "Error when creating a reserve")
+    if @response[:error].nil?
+      reserve = Reserve.add(borrowernumber: borrowernumber, branchcode: branchcode, biblionumber: biblionumber, itemnumber: itemnumber)
+      if reserve
+        @response[:reserve] = reserve.as_json
+      else
+        error_msg(ErrorCodes::OBJECT_ERROR, "Error when creating a reserve")
+      end
     end
+
     render_json
   end
 end
--- spec/controllers/api/reserves_controller_spec.rb
+++ spec/controllers/api/reserves_controller_spec.rb
@@ -3,14 +3,40 @@ require 'rails_helper'
 RSpec.describe Api::ReservesController, type: :controller do
 
   describe "get create" do
+    context "for a missing user_id" do
+      it "should return an error object" do
+        post :create, params: {location_id: 10, biblio_id: 50, loan_type: 1}
+        expect(json['error']).to_not be nil
+      end
+    end
+    context "for a missing location_id" do
+      it "should return an error object" do
+        post :create, params: {user_id: 1, biblio_id: 50, loan_type: 1}
+        expect(json['error']).to_not be nil
+      end
+    end
+    context "for a missing biblio_id" do
+      it "should return an error object" do
+        post :create, params: {user_id: 1, location_id: 10, loan_type: 1}
+        expect(json['error']).to_not be nil
+      end
+    end
+    context "for a missing loan_type" do
+      it "should return an error object" do
+        post :create, params: {user_id: 1, location_id: 10, biblio_id: 50}
+        expect(json['error']).to_not be nil
+      end
+    end
+
     context "for an invalid reservation" do
       before :each do
-        WebMock.stub_request(:get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=999&branchcode=10&itemnumber=&password=password&userid=username").
+        WebMock.stub_request(
+          :get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=&branchcode=10&itemnumber=&password=password&userid=username").
           with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com'}).
-          to_return(:status => 403, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-cannot-be-placed.xml"), :headers => {})
+          to_return(:status => 403, :body => "", :headers => {})
       end
       it "should return an error object" do
-        post :create, params: {borrower_number: 999, location_id: 10, biblio_id: 50}
+        post :create, params: {user_id: 9, location_id: 10, biblio_id: 50}
         expect(json['error']).to_not be nil
       end
     end
@@ -18,11 +44,11 @@ RSpec.describe Api::ReservesController, type: :controller do
     context "for a valid reservation" do
       before :each do
         WebMock.stub_request(:get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=1&branchcode=10&itemnumber=&password=password&userid=username").
-          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com'}).
-          to_return(:status => 201, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-success.xml"), :headers => {})
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com', 'User-Agent'=>'rest-client/2.0.0 (darwin16.1.0 x86_64) ruby/2.3.1p112'}).
+          to_return(:status => 201, :body => "", :headers => {})
       end
       it "should return a reserve object" do
-        post :create, params: {borrower_number: 1, location_id: 10, biblio_id: 50}
+        post :create, params: {user_id: 1, location_id: 10, biblio_id: 50, loan_type: 1}
         expect(json['reserve']).to_not be nil
       end
     end
