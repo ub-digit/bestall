3ba9297e66271261f0ab7414ec851e75c8aedb87
magnuskull <magnus.kull@gmail.com>
2017-02-21 16:45:37 +0100

    Update reserves_controller_spec.rb

--- spec/controllers/api/reserves_controller_spec.rb
+++ spec/controllers/api/reserves_controller_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe Api::ReservesController, type: :controller do
     context "for a valid reservation" do
       before :each do
         WebMock.stub_request(:get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=1&branchcode=10&itemnumber=&password=password&userid=username").
-          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com', 'User-Agent'=>'rest-client/2.0.0 (darwin16.1.0 x86_64) ruby/2.3.1p112'}).
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com'}).
           to_return(:status => 201, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-success.xml"), :headers => {})
       end
       it "should return a reserve object" do
