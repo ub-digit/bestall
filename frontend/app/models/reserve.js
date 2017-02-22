6d11162a6fb3e6a801892295e7263e8c9a3a2199
Magnus Kull <magnus.kull@ub.gu.se>
2017-02-22 07:49:45 +0100

    change name from comment to note

--- frontend/app/models/reserve.js
+++ frontend/app/models/reserve.js
@@ -7,6 +7,6 @@ export default DS.Model.extend({
   loanType: DS.belongsTo('loan-type', {async: false, inverse: null}),
   biblio: DS.belongsTo('biblio', {async: false, inverse: null}),
   item: DS.belongsTo('item', {async: false, inverse: null}),
-  comment: DS.attr('string')
+  note: DS.attr('string')
 
 });
