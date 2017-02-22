fa36ceb245dc52eb47b95aad57fd435e2c93b4a6
Johan Andersson von Geijer <Johan.Andersson.von.geijer@ub.gu.se>
2017-02-22 08:43:42 +0100

    KOHA-339 Change parameter name to loan_type_id
    This was done in order to follow ember model naming conventions.

--- app/controllers/api/reserves_controller.rb
+++ app/controllers/api/reserves_controller.rb
@@ -4,13 +4,12 @@ class Api::ReservesController < ApplicationController
     branchcode = params[:location_id]
     biblionumber = params[:biblio_id]
     itemnumber = params[:item_id]
-    loantype = params[:loan_type]
+    loantype = params[:loan_type_id]
 
     error_msg(ErrorCodes::VALIDATION_ERROR, "user_id is required") if borrowernumber.blank?
     error_msg(ErrorCodes::VALIDATION_ERROR, "location_id is required") if branchcode.blank?
     error_msg(ErrorCodes::VALIDATION_ERROR, "biblio_id is required") if biblionumber.blank?
-    #error_msg(ErrorCodes::VALIDATION_ERROR, "item_id is required") if itemnumber.blank?
-    error_msg(ErrorCodes::VALIDATION_ERROR, "loan_type is required") if loantype.blank?
+    error_msg(ErrorCodes::VALIDATION_ERROR, "loan_type_id is required") if loantype.blank?
 
     if @response[:error].nil?
       reserve = Reserve.add(borrowernumber: borrowernumber, branchcode: branchcode, biblionumber: biblionumber, itemnumber: itemnumber)
--- spec/controllers/api/reserves_controller_spec.rb
+++ spec/controllers/api/reserves_controller_spec.rb
@@ -5,26 +5,41 @@ RSpec.describe Api::ReservesController, type: :controller do
   describe "get create" do
     context "for a missing user_id" do
       it "should return an error object" do
-        post :create, params: {location_id: 10, biblio_id: 50, loan_type: 1}
+        post :create, params: {location_id: 10, biblio_id: 50, loan_type_id: 1}
         expect(json['error']).to_not be nil
+        expect(json['error']['code']).to eq('VALIDATION_ERROR')
+        expect(json['error']['msg']).to eq('user_id is required')
       end
     end
     context "for a missing location_id" do
       it "should return an error object" do
-        post :create, params: {user_id: 1, biblio_id: 50, loan_type: 1}
+        post :create, params: {user_id: 1, biblio_id: 50, loan_type_id: 1}
         expect(json['error']).to_not be nil
+        expect(json['error']['code']).to eq('VALIDATION_ERROR')
+        expect(json['error']['msg']).to eq('location_id is required')
       end
     end
     context "for a missing biblio_id" do
       it "should return an error object" do
-        post :create, params: {user_id: 1, location_id: 10, loan_type: 1}
+        post :create, params: {user_id: 1, location_id: 10, loan_type_id: 1}
         expect(json['error']).to_not be nil
+        expect(json['error']['code']).to eq('VALIDATION_ERROR')
+        expect(json['error']['msg']).to eq('biblio_id is required')
       end
     end
-    context "for a missing loan_type" do
+    context "for a missing loan_type_id" do
       it "should return an error object" do
         post :create, params: {user_id: 1, location_id: 10, biblio_id: 50}
+        pp json
         expect(json['error']).to_not be nil
+        expect(json['error']['code']).to eq('VALIDATION_ERROR')
+        expect(json['error']['msg']).to eq('loan_type_id is required')
+
+        # error_msg(ErrorCodes::VALIDATION_ERROR, "user_id is required") if borrowernumber.blank?
+        # error_msg(ErrorCodes::VALIDATION_ERROR, "location_id is required") if branchcode.blank?
+        # error_msg(ErrorCodes::VALIDATION_ERROR, "biblio_id is required") if biblionumber.blank?
+        # #error_msg(ErrorCodes::VALIDATION_ERROR, "item_id is required") if itemnumber.blank?
+        # error_msg(ErrorCodes::VALIDATION_ERROR, "loan_type is required") if loantype.blank?
       end
     end
 
@@ -48,7 +63,7 @@ RSpec.describe Api::ReservesController, type: :controller do
           to_return(:status => 201, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-success.xml"), :headers => {})
       end
       it "should return a reserve object" do
-        post :create, params: {user_id: 1, location_id: 10, biblio_id: 50, loan_type: 1}
+        post :create, params: {user_id: 1, location_id: 10, biblio_id: 50, loan_type_id: 1}
         expect(json['reserve']).to_not be nil
       end
     end
