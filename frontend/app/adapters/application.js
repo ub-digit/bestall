fbc6490e9afb8c52363911e9ccc5abd4c2f769e3
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2017-02-23 13:59:14 +0100

    Update application.js

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -15,13 +15,13 @@ export default ActiveModelAdapter.extend({
   handleResponse(status, header, payload) {
     if (404 === status) {
       return {status: "404",
-              errors:payload.error};
+              errors:payload.errors};
     }
 
      if (403 === status) {
        console.log('payload in adapter', payload);
       return {status: "403",
-               errors:payload.error};
+               errors:payload.errors};
     }
     return this._super(...arguments);
   }
