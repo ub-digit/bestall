fcb25d387292f4f39c04db690bb9f936dc5bfc9c
Per Larsson <per.larsson@ub.gu.se>
2017-02-23 13:42:06 +0100

    Added validate token in create reserve and some bug fixes

--- app/controllers/api/reserves_controller.rb
+++ app/controllers/api/reserves_controller.rb
@@ -1,11 +1,13 @@
 class Api::ReservesController < ApplicationController
+  before_action :validate_access
+
   def create
-    borrowernumber = params[:user_id]
-    branchcode = params[:location_id]
-    biblionumber = params[:biblio_id]
-    itemnumber = params[:item_id]
-    loantype = params[:loan_type_id]
-    reservenotes = params[:reserve_notes]
+    borrowernumber = params[:reserve][:user_id]
+    branchcode = params[:reserve][:location_id]
+    biblionumber = params[:reserve][:biblio_id]
+    itemnumber = params[:reserve][:item_id]
+    loantype = params[:reserve][:loan_type_id]
+    reservenotes = params[:reserve][:reserve_notes]
 
     error_msg(ErrorCodes::VALIDATION_ERROR, "user_id is required") if borrowernumber.blank?
     error_msg(ErrorCodes::VALIDATION_ERROR, "location_id is required") if branchcode.blank?
@@ -14,6 +16,19 @@ class Api::ReservesController < ApplicationController
       error_msg(ErrorCodes::VALIDATION_ERROR, "loan_type_id is required")
     else
       reservenotes = 'loantype: ' + loantype + '%0A' + (reservenotes.present? ? reservenotes : '')
+    end
+    if @response[:error].present?
+      render_json
+      return
+    end
+
+    # Check that requested user is same as current user
+    user_id = borrowernumber.to_i
+    currentuser_id = AccessToken.find_by_username(@current_username).user_id
+    if user_id != currentuser_id
+      error_msg(ErrorCodes::PERMISSION_ERROR, "Requested user must be same same as logged in user")
+      render_json
+      return
     end
 
     if @response[:errors].nil?
--- spec/controllers/api/reserves_controller_spec.rb
+++ spec/controllers/api/reserves_controller_spec.rb
@@ -3,9 +3,38 @@ require 'rails_helper'
 RSpec.describe Api::ReservesController, type: :controller do
 
   describe "get create" do
+    before :each do
+      WebMock.stub_request(:get, "http://koha.example.com/members/get?borrower=xallowed&password=password&userid=username").
+        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com'}).
+        to_return(:status => 200, :body => File.new("#{Rails.root}/spec/support/patron/patron-allowed.xml"), :headers => {})
+      @xallowed_token = AccessToken.generate_token(User.find_by_username('xallowed'))
+    end
+
+    context "for a valid reservation without token" do
+      it "should return an authentication error" do
+        post :create, params: {reserve: {user_id: 1, location_id: 10, biblio_id: 50, loan_type_id: 1}}
+        expect(json['error']).to_not be nil
+        expect(json['error']['code']).to eq('AUTH_ERROR')
+      end
+    end
+
+    context "for a valid reservation with an invalid token" do
+      before :each do
+        WebMock.stub_request(:get, "http://koha.example.com/members/get?borrower=xdenied&password=password&userid=username").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com', 'User-Agent'=>'rest-client/2.0.0 (linux-gnu x86_64) ruby/2.3.1p112'}).
+          to_return(:status => 200, :body => File.new("#{Rails.root}/spec/support/patron/patron-denied.xml"), :headers => {})
+        @xdenied_token = AccessToken.generate_token(User.find_by_username('xdenied'))
+      end
+      it "should return an permission error" do
+        post :create, params: {reserve: {user_id: 1, location_id: 10, biblio_id: 50, loan_type_id: 1}, token: @xdenied_token.token}
+        expect(json['error']).to_not be nil
+        expect(json['error']['code']).to eq('PERMISSION_ERROR')
+      end
+    end
+
     context "for a missing user_id" do
       it "should return an error object" do
-        post :create, params: {location_id: 10, biblio_id: 50, loan_type_id: 1}
+        post :create, params: {reserve: {location_id: 10, biblio_id: 50, loan_type_id: 1}, token: @xallowed_token.token}
         expect(json['error']).to_not be nil
         expect(json['error']['code']).to eq('VALIDATION_ERROR')
         expect(json['error']['msg']).to eq('user_id is required')
@@ -13,7 +42,7 @@ RSpec.describe Api::ReservesController, type: :controller do
     end
     context "for a missing location_id" do
       it "should return an error object" do
-        post :create, params: {user_id: 1, biblio_id: 50, loan_type_id: 1}
+        post :create, params: {reserve: {user_id: 1, biblio_id: 50, loan_type_id: 1}, token: @xallowed_token.token}
         expect(json['error']).to_not be nil
         expect(json['error']['code']).to eq('VALIDATION_ERROR')
         expect(json['error']['msg']).to eq('location_id is required')
@@ -21,7 +50,7 @@ RSpec.describe Api::ReservesController, type: :controller do
     end
     context "for a missing biblio_id" do
       it "should return an error object" do
-        post :create, params: {user_id: 1, location_id: 10, loan_type_id: 1}
+        post :create, params: {reserve: {user_id: 1, location_id: 10, loan_type_id: 1}, token: @xallowed_token.token}
         expect(json['error']).to_not be nil
         expect(json['error']['code']).to eq('VALIDATION_ERROR')
         expect(json['error']['msg']).to eq('biblio_id is required')
@@ -29,34 +58,21 @@ RSpec.describe Api::ReservesController, type: :controller do
     end
     context "for a missing loan_type_id" do
       it "should return an error object" do
-        post :create, params: {user_id: 1, location_id: 10, biblio_id: 50}
+        post :create, params: {reserve: {user_id: 1, location_id: 10, biblio_id: 50}, token: @xallowed_token.token}
         expect(json['error']).to_not be nil
         expect(json['error']['code']).to eq('VALIDATION_ERROR')
         expect(json['error']['msg']).to eq('loan_type_id is required')
       end
     end
 
-    context "for an invalid reservation" do
-      before :each do
-        WebMock.stub_request(
-          :get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=&branchcode=10&itemnumber=&password=password&userid=username").
-          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com'}).
-          to_return(:status => 403, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-borrower-not-found.xml"), :headers => {})
-      end
-      it "should return an error object" do
-        post :create, params: {user_id: 9, location_id: 10, biblio_id: 50}
-        expect(json['error']).to_not be nil
-      end
-    end
-
-    context "for a valid reservation" do
+    context "for a valid reservation with a valid token" do
       before :each do
         WebMock.stub_request(:get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=1&branchcode=10&itemnumber=&password=password&reservenotes=loantype:%201%0A&userid=username").
-         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com'}).
-         to_return(:status => 201, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-success.xml"), :headers => {})
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com', 'User-Agent'=>'rest-client/2.0.0 (linux-gnu x86_64) ruby/2.3.1p112'}).
+          to_return(:status => 201, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-success.xml"), :headers => {})
       end
       it "should return a reserve object" do
-        post :create, params: {user_id: 1, location_id: 10, biblio_id: 50, loan_type_id: 1}
+        post :create, params: {reserve: {user_id: 1, location_id: 10, biblio_id: 50, loan_type_id: 1}, token: @xallowed_token.token}
         expect(json['reserve']).to_not be nil
       end
     end
