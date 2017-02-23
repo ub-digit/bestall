b658bc80aa1916181465a2f02cbf8a9fd87287d3
Johan Andersson von Geijer <Johan.Andersson.von.geijer@ub.gu.se>
2017-02-23 07:55:00 +0100

    Handle error messages from Koha.
    - CChanged code
    - Changed tests

--- app/controllers/api/reserves_controller.rb
+++ app/controllers/api/reserves_controller.rb
@@ -5,16 +5,31 @@ class Api::ReservesController < ApplicationController
     biblionumber = params[:biblio_id]
     itemnumber = params[:item_id]
     loantype = params[:loan_type_id]
+    reservenotes = params[:reserve_notes]
 
     error_msg(ErrorCodes::VALIDATION_ERROR, "user_id is required") if borrowernumber.blank?
     error_msg(ErrorCodes::VALIDATION_ERROR, "location_id is required") if branchcode.blank?
     error_msg(ErrorCodes::VALIDATION_ERROR, "biblio_id is required") if biblionumber.blank?
-    error_msg(ErrorCodes::VALIDATION_ERROR, "loan_type_id is required") if loantype.blank?
+    if loantype.blank?
+      error_msg(ErrorCodes::VALIDATION_ERROR, "loan_type_id is required")
+    else
+      reservenotes = 'loantype: ' + loantype + '%0A' + (reservenotes.present? ? reservenotes : '')
+    end
 
     if @response[:error].nil?
-      reserve = Reserve.add(borrowernumber: borrowernumber, branchcode: branchcode, biblionumber: biblionumber, itemnumber: itemnumber)
-      if reserve
-        @response[:reserve] = reserve.as_json
+      result = Reserve.add(borrowernumber: borrowernumber, branchcode: branchcode, biblionumber: biblionumber, itemnumber: itemnumber, reservenotes: reservenotes)
+      if result.class == Reserve
+        @response[:reserve] = result.as_json
+      elsif result.class == Hash
+        if result[:code][:http_status] == 400
+          error_msg(ErrorCodes::ERROR, result[:msg], result[:errors])
+        end
+        if result[:code][:http_status] == 403
+          error_msg(ErrorCodes::PERMISSION_ERROR, result[:msg], result[:errors])
+        end
+        if result[:code][:http_status] == 404
+          error_msg(ErrorCodes::REQUEST_ERROR, result[:msg], result[:errors])
+        end
       else
         error_msg(ErrorCodes::OBJECT_ERROR, "Error when creating a reserve")
       end
--- spec/models/reserve_spec.rb
+++ spec/models/reserve_spec.rb
@@ -4,54 +4,52 @@ RSpec.describe Reserve, type: :model do
   describe "add" do
     context "for an invalid reservation" do
       before :each do
-        WebMock.stub_request(:get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=999&branchcode=10&itemnumber=&password=password&userid=username").
+        WebMock.stub_request(:get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=999&branchcode=10&itemnumber=&password=password&reservenotes=loan_type:%201%0Abla%20dih%20blah&userid=username").
           with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com'}).
           to_return(:status => 403, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-cannot-be-placed.xml"), :headers => {})
       end
       it "should not return a Reserve object" do
-        reserve = Reserve.add(borrowernumber: 999, branchcode: 10, biblionumber: 50)
+        reserve = Reserve.add(borrowernumber: 999, branchcode: 10, biblionumber: 50, reservenotes: "loan_type: 1%0Abla dih blah")
         expect(reserve).to be_falsey
       end
     end
 
     context "for a valid reservation" do
       before :each do
-        WebMock.stub_request(:get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=1&branchcode=10&itemnumber=&password=password&userid=username").
+        WebMock.stub_request(:get, "http://koha.example.com/reserves/create?biblionumber=50&borrowernumber=1&branchcode=10&itemnumber=&password=password&reservenotes=loan_type:%201%0Abla%20dih%20blah&userid=username").
           with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'koha.example.com'}).
           to_return(:status => 201, :body => File.new("#{Rails.root}/spec/support/reserve/reserve-success.xml"), :headers => {})
       end
       it "should return a Reserve object" do
-        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50)
+        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50, reservenotes: "loan_type: 1%0Abla dih blah")
         expect(reserve).to be_truthy
       end
       it "should return id" do
-        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50)
+        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50, reservenotes: "loan_type: 1%0Abla dih blah")
         expect(reserve.id).to be_truthy
         expect(reserve.id).to eq 100
 
       end
       it "should return borrowernumber" do
-        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50)
+        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50, reservenotes: "loan_type: 1%0Abla dih blah")
         expect(reserve.borrowernumber).to be_truthy
       end
       it "should return branchcode" do
-        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50)
+        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50, reservenotes: "loan_type: 1%0Abla dih blah")
         expect(reserve.branchcode).to be_truthy
       end
       it "should return biblionumber" do
-        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50)
+        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50, reservenotes: "loan_type: 1%0Abla dih blah")
         expect(reserve.biblionumber).to be_truthy
       end
       it "should return reservedate" do
-        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50)
+        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50, reservenotes: "loan_type: 1%0Abla dih blah")
         expect(reserve.reservedate).to be_truthy
       end
       it "should return timestamp" do
-        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50)
+        reserve = Reserve.add(borrowernumber: 1, branchcode: 10, biblionumber: 50, reservenotes: "loan_type: 1%0Abla dih blah")
         expect(reserve.timestamp).to be_truthy
       end
     end
   end
 end
-
-
