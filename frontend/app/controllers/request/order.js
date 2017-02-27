2bf6ce998415909627b6a1ae8a57675b59eefed0
Magnus Kull <magnus.kull@ub.gu.se>
2017-02-27 12:58:59 +0100

    set default loan type

--- frontend/app/controllers/request/order.js
+++ frontend/app/controllers/request/order.js
@@ -1,8 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	i18n: Ember.inject.service(),
-	// queryParams: ['location', 'type'],
-	location: null,
-	type: null
+	i18n: Ember.inject.service()	
 });
