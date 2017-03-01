4962694091827556c1a195b318aa30353fe751f2
Magnus Kull <magnus.kull@ub.gu.se>
2017-03-01 23:59:27 +0100

    que and order buttons implement rules

--- frontend/app/controllers/request/order/items.js
+++ frontend/app/controllers/request/order/items.js
@@ -5,8 +5,12 @@ export default Ember.Controller.extend({
   request: Ember.inject.controller(),
   order: Ember.inject.controller('request.order'),
 
+  showQueueOnBiblio: Ember.computed('request.model.biblio.{canBeQueuedOnItem,canBeQueued}', function() {
+    return !this.get('request.model.biblio.canBeQueuedOnItem') && this.get('request.model.biblio.canBeQueued');
+  }),
+
   actions: {
-    setItem(item) {
+    orderItem(item) {
       this.get('order.model.reserve').set('item', item);
       this.transitionToRoute('request.order.details');
     }
