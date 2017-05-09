c931989dba0b3b4043724864d0b8cb76ac92c73d
Johan Larsson <johan.larsson@ub.gu.se>
2017-05-09 18:10:06 +0200

    added a choice for inputtype in component and hided the not applicable

--- frontend/app/components/reserve-note-input.js
+++ frontend/app/components/reserve-note-input.js
@@ -4,5 +4,12 @@ export default Ember.Component.extend({
 
   inputLength: Ember.computed('value', function() {
     return this.get('value.length') || 0;
+  }),
+
+  isTextarea: Ember.computed('type', function() {
+    if (this.get("type") === "textarea") {
+      return true;
+    }
+    return false;
   })
 });
