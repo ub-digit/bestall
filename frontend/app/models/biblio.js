4d00b666c04dbf5b0d78401c28bdbf967cb96c33
Per Larsson <per.larsson@ub.gu.se>
2017-05-18 15:47:56 +0200

    Added missing file

--- frontend/app/models/biblio.js
+++ frontend/app/models/biblio.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   author: DS.attr(),
+  place: DS.attr(),
   canBeQueued: DS.attr('boolean'),
   hasItemLevelQueue: DS.attr('boolean'),
   recordType: DS.attr('string'),
