594dc79cf47652d1b976f6aafd69c73abddf5eff
Johan Andersson von Geijer <johan.andersson.von.geijer@ub.gu.se>
2017-10-19 17:35:27 +0200

    KOHA-701: Fix of pickuplocation decision code.
    Fixed the non-working code adding similarly bad code.
    It is fixed it for now, but needs refactoring.

--- frontend/app/controllers/request/order/details.js
+++ frontend/app/controllers/request/order/details.js
@@ -2,51 +2,93 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  request: Ember.inject.controller(),
+  request: Ember.inject.controller('request'),
   order: Ember.inject.controller('request.order'),
 
   possiblePickupLocations: Ember.computed.filterBy('order.model.locations', 'isPickupLocation', true),
 
-  pickupLocations: Ember.computed('order', 'possiblePickupLocations', function() {
+  applyFilter: Ember.computed('order.model.reserve.biblio', 'order.model.reserve.item', function() {
+    let recordType = this.get('order.model.reserve.biblio.recordType');
 
+    if (recordType === 'monograph' && (this.get('order.model.reserve.item') != null)) {
+      //console.log('Reservationens biblio är en monografi OCH reservationen har ett exemplar. -> FILTRERA!');
+      //console.log('Item ID: ' + this.get('order.model.reserve.item').id + ' Barcode: ' + this.get('order.model.reserve.item').barcode);
+      return true;
+    }
+    if (recordType === 'serial' && (this.get('order.model.reserve.item') != null) && (this.get('order.model.reserve.item.canBeOrdered'))) {
+      //console.log('Reservationens biblio är en periodika OCH reservationen har ett exemplar OCH reservationens exemplar kan beställas. -> FILTRERA!');
+      //console.log('Item ID: ' + this.get('order.model.reserve.item').id + ' Barcode: ' + this.get('order.model.reserve.item').barcode);
+      return true;
+    }
+    //console.log('Reservationen är kö eller beståndsbeställning. -> INGET FILTER!');
+    return false;
+  }),
+
+  pickupLocations: Ember.computed('order.model.reserve.{subscription,user.userCategory,item}', 'possiblePickupLocations', 'applyFilter', function() {
+    //console.log('DEBUG: IN pickupLocations');
     // the current users userCategory
     const userCategory = this.get('order.model.reserve.user.userCategory');
     //All possible pickup locations
     let locations = this.get('possiblePickupLocations');
+
+    // Reset the items in the locations array since they might have been
+    // disabled earlier.
+    locations.map((item) => {
+      item.set('disabled', false);
+    });
+
+    //let applyFilter = this.get('order.model.reserve.applyFilter');
+    let filter = this.get('applyFilter');
+    if (!filter) {
+      //console.log('NO FILTER!!! -> applyFilter is ' + filter);
+      return locations;
+    } else {
+      //console.log('APPLY THE FILTER!!! -> applyFilter is ' + filter);
+    }
+
     let entity = this.get('order.model.reserve.subscription') ? this.get('order.model.reserve.subscription') : this.get('order.model.reserve.item');
     // if there is no item or subscription return all pickup locations
     if (!entity) {
+      //console.log('DEBUG: No Entity -> no subscription and no item');
       return locations;
     }
 
     let isOpenLoc = entity.get('sublocation.isOpenLoc');
     let isOpenPickupLoc = entity.get('sublocation.isOpenPickupLoc');
+    //console.log('DEBUG: isOpenPickupLoc==' + isOpenPickupLoc);
+    //console.log('DEBUG: isOpenLoc==' + isOpenLoc);
     // I items is OPEN_PICKUP_LOC, return all locations.
     // Nope, item can be booth OPEN_LOC and OPEN_PICKUP_LOC.
     if (isOpenPickupLoc) {
+      //console.log('DEBUG: isOpenPickupLoc was true');
       return locations;
     }
     if (isOpenLoc) {
       // Only FI, SY, FU users can pickup items at its home/current location, thus return all locations
       if (['FI', 'SY', 'FY'].includes(userCategory)) {
+        //console.log('DEBUG: isOpenLoc was TRUE and user is FI, SI or FY!');
         return locations;
       } else {
         // Elser filter out the home/current location from available locations
 
         const homeLocation = entity.get('sublocation.location.id');
         const filteredLocations = [];
+        //klona arrayen istället och sätt värdet på kloonen istället för att
+        //sätta värdet på referensen till originalobjektet
+        ////let newObject = Object.assign({}, oldObject)
         locations.map((item) => {
           item.set('disabled', false);
           const id = item.get('id');
           if (id == homeLocation) {
             item.set('disabled', true);
-
           }
           filteredLocations.push(item);
         });
+        //console.log('DEBUG: isOpenLoc is TRUE! and ordinary patron category.');
         return filteredLocations;
       }
     } else {
+      //console.log('DEBUG: isOpenLoc was FALSE!');
       return locations;
     }
 
