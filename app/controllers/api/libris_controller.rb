e0cf87b4726cdf241c6097c6e13cae722f48ab6c
Per Larsson <per.larsson@ub.gu.se>
2018-04-10 17:06:32 +0200

    Fix encoding errors in Libis item info xml

--- app/controllers/api/libris_controller.rb
+++ app/controllers/api/libris_controller.rb
@@ -1,4 +1,3 @@
-# encoding: ISO-8859-1
 class Api::LibrisController < ApplicationController
   def index
     librisid = params[:Bib_ID]
@@ -17,20 +16,24 @@ class Api::LibrisController < ApplicationController
         end
       end
     end
-    xml_document = print_document items_xml
+    xml_document = print_document items_xml.encode("ISO-8859-1")
     render xml: xml_document
   end
 
   def get_bibid librisid
-    return nil if librisid.blank?
-    base_url = APP_CONFIG['koha']['base_url']
-    user =  APP_CONFIG['koha']['user']
-    password =  APP_CONFIG['koha']['password']
-    url = "#{base_url}/librisid2bibid/?librisid=#{librisid}&userid=#{user}&password=#{password}"
-    response = RestClient.get url
-    bibid = JSON.parse(response.body)["bibid"]
-    return nil if bibid.blank?
-    return bibid
+    begin
+      return nil if librisid.blank?
+      base_url = APP_CONFIG['koha']['base_url']
+      user =  APP_CONFIG['koha']['user']
+      password =  APP_CONFIG['koha']['password']
+      url = "#{base_url}/librisid2bibid/?librisid=#{librisid}&userid=#{user}&password=#{password}"
+      response = RestClient.get url
+      bibid = JSON.parse(response.body)["bibid"]
+      return nil if bibid.blank?
+      return bibid
+    rescue StandardError => error
+      return nil
+    end
   end
 
   def print_item item, index
@@ -38,12 +41,12 @@ class Api::LibrisController < ApplicationController
     xml << "<Item>"
     xml << "<Item_No>#{index + 1}</Item_No>"
     xml << "<UniqueItem_ID>#{item.id}</UniqueItem_ID>"
-    xml << "<Location>#{item.location_name_sv}</Location>"
-    xml << "<Call_No>#{item.item_call_number}</Call_No>"
+    xml << "<Location>#{fix_diacritics item.location_name_sv}</Location>"
+    xml << "<Call_No>#{fix_diacritics item.item_call_number}</Call_No>"
     xml << "<Status>#{get_status item.status}</Status>"
     xml << "<Loan_Policy>#{get_loan_policy item.item_type}</Loan_Policy>"
     if item.status == "LOANED" && item.due_date
-      xml << "<Status_Date_Description>Åter:</Status_Date_Description>"
+      xml << "<Status_Date_Description>&#197;ter:</Status_Date_Description>"
       xml << "<Status_Date>#{Time.parse(item.due_date).strftime("%Y-%m-%d")}</Status_Date>"
    end
     xml << "</Item>"
@@ -59,15 +62,15 @@ class Api::LibrisController < ApplicationController
   def get_loan_policy item_type
     case item_type.to_i
     when 1, 3, 18
-      return "Hemlån"
+      return "Heml&#229;n"
     when 8
-      return "Ej hemlån"
+      return "Ej heml&#229;n"
     when 2, 19, 21
-      return "Ej fjärrlån"
+      return "Ej fj&#228;rrl&#229;n"
     when 4, 5, 6, 9, 10, 11, 20, 22, 23
       return "Tidskrift"
     when 7, 12, 13, 14, 15, 16
-      return "Utlånas ej"
+      return "Utl&#229;nas ej"
     when 17
       return "Mikrofilm"
     else
@@ -76,14 +79,14 @@ class Api::LibrisController < ApplicationController
   end
 
   def get_status code
-    return "Utlånad" if code == "LOANED"
-    return "Väntar på avhämtning" if code == "WAITING"
+    return "Utl&#229;nad" if code == "LOANED"
+    return "V&#228;ntar p&#229; avh&#228;mtning" if code == "WAITING"
     return "Under transport" if code == "IN_TRANSIT"
     return "Reserverad" if code == "RESERVED"
-    return "Försenad" if code == "DELAYED"
-    return "Ej på plats" if code == "NOT_IN_PLACE"
-    return "Under inköp" if code == "DURING_ACQUISITION"
-    "Tillgänglig"
+    return "F&#246;rsenad" if code == "DELAYED"
+    return "Ej p&#229; plats" if code == "NOT_IN_PLACE"
+    return "Under ink&#246;p" if code == "DURING_ACQUISITION"
+    "Tillg&#228;nglig"
   end
 
   def get_location_id sigel
@@ -98,5 +101,15 @@ class Api::LibrisController < ApplicationController
     return 60 if sigel == "Ghdk"
     return 62 if sigel == "Gumu"
     nil
+  end
+
+  def fix_diacritics str
+    str = str.gsub('Å', '&#197;')
+    str = str.gsub('å', '&#229;')
+    str = str.gsub('Ä', '&#196;')
+    str = str.gsub('ä', '&#228;')
+    str = str.gsub('Ö', '&#214;')
+    str = str.gsub('ö', '&#246;')
+    return str
   end
 end
