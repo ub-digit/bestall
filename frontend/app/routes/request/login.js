bc8605cf7a4a6db63fe55182421e3e0f140d100a
David  Gustafsson <david.gustafsson@ub.gu.se>
2024-10-14 17:21:41 +0200

    Revert commit 7019731 and remove remaining cas code

--- frontend/app/routes/request/login.js
+++ frontend/app/routes/request/login.js
@@ -4,23 +4,10 @@ export default Ember.Route.extend({
   model: function(params, transition) {
     var biblioId = transition.params.request.id;
     this.get('store').createRecord('tmp_biblio', {id: 1, biblio: biblioId});
-
-    return new Ember.RSVP.Promise((resolve) => {
-      localStorage.removeItem('login-check');
-      resolve();
-    });
+    //??
   },
   setupController: function(controller) {
     controller.set('showForm', true);
   },
-
-  returnUrl: function(id) {
-    let baseUrl = window.location.origin;
-    let routeUrl = this.router.generate('request', {id: id}, {queryParams: {SSOscanner: null}});
-    return baseUrl + routeUrl;
-  },
-  casLoginUrl() {
-    return this.get('store').peekRecord('config', 1).get('casurl') + '/login';
-  }
 
 });
