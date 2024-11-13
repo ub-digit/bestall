ub-digit/bestall

commit de1f361a85b5db4a9496326dfdcfbc75472bf3e2

Remove GU-login for Koha-view

frontend/app/controllers/request/login.js
@@ -33,6 +33,9 @@ export default Ember.Controller.extend({
   inputAutocomplete: Ember.computed(function() {
     return (this.get('request.view') !== '46GUB_KOHA');
   }),
+  showGULogin: Ember.computed(function() {
+    return (this.get('request.view') === '46GUB_KOHA');
+  }),
   libraryCardUrl: Ember.computed(function() {
     var lang = this.get('getLocale');
     return this.get('store').peekRecord('config', 1).get('registrationurl') + '?lang=' + lang;

frontend/app/templates/request/login.hbs
@@ -3,6 +3,7 @@
     <div class="login-wrapper">
       <div class="row">
         <div class="col-xs-12 col-sm-6 col-sm-offset-3">
+          {{#if showGULogin}}
           <div class="gu-account-login">
             <a href="javascript:void()" {{action "loginOAuth2"}} id="gu-account-login-link">
               <div class="panel panel-default">
@@ -32,6 +33,7 @@
               </div>
             </a>
           </div>
+          {{/if}}
           <div class="alternative-wrapper">
             {{#if oauth2ErrorMessage}}
               <div class="alert alert-danger col-xs-12">{{oauth2ErrorMessage}}</div>
@@ -40,7 +42,9 @@
               <span class="word">{{t 'request.login.or'}}</span>
             </div>
             <div class="gu-login-account-heading">{{t 'request.login.login-account-heading'}}</div>
+            {{#if showGULogin}}
             <span class="gu-login-account-body">{{t 'request.login.login-account-body'}}</span>
+            {{/if}}
             {{#if usernamePasswordErrorMessage}}
               <div class="alert alert-danger col-xs-12">{{usernamePasswordErrorMessage}}</div>
             {{/if}}

commit 13ba208361def8c1bd3bf43f8a5f476be592f959

Fix issues when disable GU login in Koha view was re-introduced in commit de1f361

frontend/app/controllers/request/login.js
@@ -34,7 +34,7 @@ export default Ember.Controller.extend({
     return (this.get('request.view') !== '46GUB_KOHA');
   }),
   showGULogin: Ember.computed(function() {
-    return (this.get('request.view') === '46GUB_KOHA');
+    return (this.get('request.view') !== '46GUB_KOHA');
   }),
   libraryCardUrl: Ember.computed(function() {
     var lang = this.get('getLocale');

frontend/app/templates/request/login.hbs
@@ -38,9 +38,11 @@
             {{#if oauth2ErrorMessage}}
               <div class="alert alert-danger col-xs-12">{{oauth2ErrorMessage}}</div>
             {{/if}}
+            {{#if showGULogin}}
             <div class="lines">
               <span class="word">{{t 'request.login.or'}}</span>
             </div>
+            {{/if}}
             <div class="gu-login-account-heading">{{t 'request.login.login-account-heading'}}</div>
             {{#if showGULogin}}
             <span class="gu-login-account-body">{{t 'request.login.login-account-body'}}</span>