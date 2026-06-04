8d3bfc08f85cfa27396a7a3924b1d9f91ede5256
Johan Larsson <johan.larsson@ub.gu.se>
2026-06-04 11:30:24 +0200

    types for orderConfirmation

--- frontend-nuxt-4/shared/types/Order.ts
+++ frontend-nuxt-4/shared/types/Order.ts
@@ -1,11 +1,22 @@
 import type { Biblio } from "./Biblio";
 
+type OrderSuccessResponse = {
+  showQueuePosition: boolean;
+  showPickupLocation: boolean;
+  showMyLoansLink: boolean;
+  showRequiredPickupCode: boolean;
+  positionInQueue: string;
+  pickupLocation_en: string;
+  pickupLocation_sv: string;
+  pickupLocation: string;
+};
+
 type Order = {
   user: string;
   location: string | null;
   loanType: number | null;
   biblio: string;
-  fullBiblio: Biblio | null; // TODO: Define a proper type for this
+  fullBiblio: Biblio | null;
   item: string;
   reserveNotes: string;
   queuePosition: string;
@@ -16,6 +27,7 @@ type Order = {
   subscriptionSublocationId: string;
   subscriptionSublocation: string;
   subscriptionCallNumber: string;
+  orderSuccessResponse?: OrderSuccessResponse | null;
 };
 
 export type { Order };
