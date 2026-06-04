31569f71ff4b2128b1b053e3cf4555ef7f7141a4
Johan Larsson <johan.larsson@ub.gu.se>
2026-06-04 11:29:30 +0200

    endpoint to check user against biblio for restrictions

---
+++ frontend-nuxt-4/server/api/currentuser.get.ts
@@ -0,0 +1,36 @@
+import { getServerSession } from "#auth";
+import { FetchError } from "ofetch";
+
+export default defineEventHandler(async (event) => {
+  const session = await getServerSession(event);
+  if (!session) {
+    throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
+  }
+  let { biblio, current_username } = getQuery(event) as {
+    biblio?: string;
+    current_username?: string;
+  };
+
+  const runtimeConfig = useRuntimeConfig();
+
+  try {
+    const response: any = await $fetch(
+      `${runtimeConfig.apiBase}/users/current?biblio=${biblio}`,
+      {
+        method: "GET",
+        headers: {
+          current_username: current_username || "",
+        },
+      },
+    );
+    return response;
+  } catch (error: FetchError | any) {
+    const msg = error.data?.errors?.errors || "Unknown error";
+    console.error("Error fetching current user data:", msg);
+    throw createError({
+      statusCode: error.status || 500,
+      statusMessage: error.statusText || "Internal Server Error",
+      data: msg,
+    });
+  }
+});
