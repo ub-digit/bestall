d40beac668fdf2186397f0ab92e467079379072a
Johan Larsson <johan.larsson@ub.gu.se>
2026-06-05 14:12:41 +0200

    prints according to orderSuccessResponse

--- frontend-nuxt-4/app/pages/order/[id]/confirm.vue
+++ frontend-nuxt-4/app/pages/order/[id]/confirm.vue
@@ -1,47 +1,52 @@
 <template>
   <div class="confirm-page">
     <UserWarning />
-
     <div class="confirmation-message">
-      <div>
-        <h3>DEBUG:</h3>
-
-        <h4>Order data sent from API:</h4>
-        <pre>{{ JSON.stringify(order, null, 2) }}</pre>
-        <h4>Locations</h4>
-        <pre>{{ JSON.stringify(locations, null, 2) }}</pre>
-        <h4>Loan Types</h4>
-        <pre>{{ JSON.stringify(loanTypes, null, 2) }}</pre>
-      </div>
-
       <h1>{{ $t("confirmation.header") }}</h1>
 
-      <div v-if="order.queuePosition" class="queue-info-wrapper">
-        {{
-          $t("confirmation.queuePosition", { position: order.queuePosition })
-        }}
-      </div>
+      <div
+        v-if="orderSuccessResponse?.showQueuePosition"
+        class="queue-info-wrapper"
+        v-html="
+          $t('confirmation.youHavePlaceInQueue', {
+            positionInQueue: orderSuccessResponse?.positionInQueue,
+          })
+        "
+      ></div>
 
       <div
-        v-if="currentLoanTypeOnOrder.value?.showPickupLocation"
+        v-if="orderSuccessResponse?.showPickupLocation"
         class="pickup-location-info"
       >
-        {{
-          $t("confirmation.messagePickup", {
-            pickupLocation: currentLocationOnOrder.value?.name,
-          })
-        }}
+        <div
+          v-html="
+            $t('confirmation.messagePickup', {
+              pickupLocation: orderSuccessResponse?.pickupLocation,
+            })
+          "
+        ></div>
+
+        <div
+          v-if="orderSuccessResponse?.showRequiredPickupCode"
+          class="pickup-code-info"
+        >
+          {{ $t("confirmation.pickupInfoCode") }}
+        </div>
+        <div
+          v-else-if="!orderSuccessResponse?.showRequiredPickupCode"
+          class="pickup-info-name"
+          v-html="$t('confirmation.pickupInfoName')"
+        ></div>
       </div>
       <div v-else class="no-pickup-info">
         {{ $t("confirmation.messageNoPickup") }}
       </div>
 
       <div
-        v-if="order.fullBiblio.hasItemLevelQueue || order.subscription"
+        v-if="orderSuccessResponse?.showMyLoansLink"
         class="my-loans-link"
-      >
-        {{ $t("confirmation.myLoansLink") }}
-      </div>
+        v-html="$t('confirmation.myLoansLink')"
+      ></div>
     </div>
   </div>
 </template>
@@ -52,7 +57,14 @@ definePageMeta({
 });
 
 const route = useRoute();
-const { order, setOrder, resetOrder } = useOrder();
+const {
+  order,
+  setOrder,
+  resetOrder,
+  orderSuccessResponse,
+  setOrderSuccessResponse,
+  resetOrderSuccessResponse,
+} = useOrder();
 if (!order.value?.biblio) {
   // if there's no biblio data in the order, we can't show the details page, so we redirect back to the search page
   const route = useRoute();
@@ -60,58 +72,21 @@ if (!order.value?.biblio) {
     useLocalePath()({ path: "/order/" + route.params.id, query: route.query }),
   );
 }
-const orderError = ref(null);
-
-const currentItemOnOrder = computed(() => {
-  if (!order.value || !order.value.item) return null;
-  return order.value.fullBiblio?.items?.find(
-    (item: Item) => item.id === order.value.item,
-  );
-});
-
-const currentSubscriptionOnOrder = computed(() => {
-  if (!order.value || !order.value.subscription) return null;
-  return order.value.fullBiblio?.subscriptiongroups
-    ?.flatMap((sg: any) => sg.subscriptions)
-    .find((sub: any) => sub.id === order.value.subscription);
-});
-
-const currentLoanTypeOnOrder = computed(() => {
-  if (!order.value || !order.value.loanType) return null;
-  return loanTypes.value?.find(
-    (loanType: LoanType) => loanType.id === order.value.loanType,
-  );
-});
-
-const currentLocationOnOrder = computed(() => {
-  if (!order.value || !order.value.pickupLocation) return null;
-  return locations.value?.find(
-    (loc: Location) => loc.id === order.value.pickupLocation,
-  );
-});
-
-const { locations, setLocations, resetLocations } = useLocations();
-const { loanTypes, setLoanTypes, resetLoanTypes } = useLoanTypes();
-
-try {
-  const { data, error } = await useFetch(`/api/orders/${route.params.id}/`, {
-    method: "POST",
-    body: {
-      order: order.value, // Pass the current order data to the API for confirmation
-    },
-  });
-  if (error.value) {
-    // Handle error case, e.g. show an error message or redirect to an error page
-    console.error("Error confirming order:", error.value);
-  }
-} catch (error) {
-  orderError.value = error; // Set the error object to display an error message in the template
-  console.error("Error confirming order:", error);
-}
 </script>
 
 <style scoped>
 .confirm-page {
+  max-width: var(--reading-width);
+  .confirmation-message {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    .pickup-location-info {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+  }
   /* Styles here */
 }
 </style>
