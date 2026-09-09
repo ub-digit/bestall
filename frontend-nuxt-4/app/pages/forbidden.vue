981f8d599c068454265576f00df23eaba3c758c6
Johan Larsson <johan.larsson@ub.gu.se>
2026-09-09 14:55:44 +0200

    forbidden route added to handle special case for user that has usercategory AV

---
+++ frontend-nuxt-4/app/pages/forbidden.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="forbidden">
+    <OrderDenied v-if="userError" :error="userError">
+      <template #description><span></span></template>
+    </OrderDenied>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data, signIn, signOut } = useAuth();
+const userError = {
+  data: {
+    data: data.value?.user?.errors.errors ?? [],
+  },
+};
+</script>
+
+<style scoped></style>
